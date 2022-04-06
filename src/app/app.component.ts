72862ccc69fc87e4cbfa215133bc09a130a56e6d
bigredboots <bigredboots@gmail.com>
2022-04-06 11:04:59 -0600

    whoknows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AppplicationRef } from '@angular/core';
 import { GlobalConstants } from './common/global-constants';
 import { Idle, DEFAULT_INTERRUPTSOURCES } from '@ng-idle/core';
 import {
@@ -16,13 +16,14 @@ import { NavigationLink } from './shared';
 export class AppComponent implements OnInit {
   idleState = 'Not started.';
   timedOut = false;
+  countdown = '';
 
   breadCrumb = GlobalConstants.breadCrumb;
 
   public configuration: DashboardLayoutConfiguration;
   public links: NavigationLink[];
 
-  constructor(private idle: Idle) {
+  constructor(private appRef: ApplicationRef, private idle: Idle) {
     this.configuration = new DashboardLayoutConfiguration(
       SidePanelPosition.LEFT,
       SidePanelState.OPEN
@@ -38,34 +39,32 @@ export class AppComponent implements OnInit {
     window.scroll(0, 0);
 
     // sets an idle timeout of 15 seconds.
-    this.idle.setIdle(15);
+    this.idle.setIdle(5);
 
     // sets an  timer for 30 seconds.
-    this.idle.setTimeout(60);
+    this.idle.setTimeout(90);
 
     // sets the default interrupts, in this case, things like clicks, scrolls, touches to the document
     this.idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
 
-    this.idle.onIdleEnd.subscribe(() => (this.idleState = 'No longer idle.'));
+    this.idle.onIdleEnd.subscribe(() => (
+      
+      this.idleState = 'No longer idle.';
+      this.appRef.tick();
+      );
     this.idle.onTimeout.subscribe(() => {
-      this.idleState = 'Timed out!';
+      this.idleState = 'timedout';
       this.timedOut = true;
     });
-    this.idle.onIdleStart.subscribe(
-      () => (this.idleState = "You've gone idle!")
-    );
+    this.idle.onIdleStart.subscribe(() => (this.idleState = 'warning'));
 
-    // this.idle.onIdleStart.subscribe(() =>
-    //   document.body.classList.add('modal-open')
-    // );
+    this.idle.onIdleStart.subscribe(() =>
+      document.body.classList.add('modal-open')
+    );
 
     // document.body.classList.add('modal-open')
     this.idle.onTimeoutWarning.subscribe(
-      (countdown) =>
-        (this.idleState =
-          'Are you still here? This is a timeout message.  The login screen will happen in ' +
-          countdown +
-          ' seconds.')
+      (countdown) => (this.countdown = countdown)
     );
 
     this.reset();
@@ -73,7 +72,8 @@ export class AppComponent implements OnInit {
 
   reset() {
     this.idle.watch();
-    this.idleState = 'Started.';
+    this.idleState = 'started.';
     this.timedOut = false;
+    document.body.classList.remove('modal-open');
   }
 }
